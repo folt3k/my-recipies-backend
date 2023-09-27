f8c74821caede68c39b3616714ca4fb753befebb
folt3k <folt3k@gmail.com>
2023-09-27 21:06:19 +0200

    conditional swagger

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import 'dotenv/config';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  const config = new DocumentBuilder()
-    .setTitle('My Recipes')
-    .setDescription('My Recipes API description')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  if (process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('My Recipes')
+      .setDescription('My Recipes API description')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
 
   await app.listen(process.env.PORT);
 }
